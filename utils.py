6dbab601bbec3551d5210517d0fbb661d1716d2e
jeongjoonsup <penny4860@gmail.com>
2019-04-10 21:52:45 +0900

    run_by_torch() added

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 
 import numpy as np
+import os
 from PIL import Image
 from cartoon import SAMPLE_IMG
 
@@ -15,4 +17,24 @@ def load_net_in(img_fname=SAMPLE_IMG, desired_size=256):
     input_image = -1 + 2 * input_image
     return input_image
 
-
+def run_by_torch(input_image):
+    import torch
+    from cartoon import MODEL_ROOT
+    def load_model():
+        model = Transformer()
+        model.load_state_dict(torch.load(os.path.join(MODEL_ROOT, 'Hayao_net_G_float.pth')))
+        model.eval()
+        model.float()
+        return model
+    from torch.autograd import Variable
+    from network.Transformer import Transformer
+    import torchvision.transforms as transforms
+    
+    model = load_model()
+    input_image = transforms.ToTensor()(input_image).unsqueeze(0)
+    input_image = Variable(input_image, volatile=True).float()
+  
+    # forward
+    ys = model(input_image)
+    return ys
+    
