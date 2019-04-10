9663adafeac4ee8d2672e6fd43bede8db545daa8
jeongjoonsup <penny4860@gmail.com>
2019-04-10 20:48:28 +0900

    InstanceNormalization() added

--- layers.py
+++ layers.py
@@ -57,21 +57,145 @@ class SpatialReflectionPadding(Layer):
                       mode="REFLECT")
 
 
-class InstanceNorm(Layer):
+class InstanceNormalization(Layer):
+    """Instance normalization layer.
+    Normalize the activations of the previous layer at each step,
+    i.e. applies a transformation that maintains the mean activation
+    close to 0 and the activation standard deviation close to 1.
+    # Arguments
+        axis: Integer, the axis that should be normalized
+            (typically the features axis).
+            For instance, after a `Conv2D` layer with
+            `data_format="channels_first"`,
+            set `axis=1` in `InstanceNormalization`.
+            Setting `axis=None` will normalize all values in each
+            instance of the batch.
+            Axis 0 is the batch dimension. `axis` cannot be set to 0 to avoid errors.
+        epsilon: Small float added to variance to avoid dividing by zero.
+        center: If True, add offset of `beta` to normalized tensor.
+            If False, `beta` is ignored.
+        scale: If True, multiply by `gamma`.
+            If False, `gamma` is not used.
+            When the next layer is linear (also e.g. `nn.relu`),
+            this can be disabled since the scaling
+            will be done by the next layer.
+        beta_initializer: Initializer for the beta weight.
+        gamma_initializer: Initializer for the gamma weight.
+        beta_regularizer: Optional regularizer for the beta weight.
+        gamma_regularizer: Optional regularizer for the gamma weight.
+        beta_constraint: Optional constraint for the beta weight.
+        gamma_constraint: Optional constraint for the gamma weight.
+    # Input shape
+        Arbitrary. Use the keyword argument `input_shape`
+        (tuple of integers, does not include the samples axis)
+        when using this layer as the first layer in a Sequential model.
+    # Output shape
+        Same shape as input.
+    # References
+        - [Layer Normalization](https://arxiv.org/abs/1607.06450)
+        - [Instance Normalization: The Missing Ingredient for Fast Stylization](
+        https://arxiv.org/abs/1607.08022)
+    """
+    def __init__(self,
+                 axis=3,
+                 epsilon=1e-3,
+                 center=True,
+                 scale=True,
+                 beta_initializer='zeros',
+                 gamma_initializer='ones',
+                 beta_regularizer=None,
+                 gamma_regularizer=None,
+                 beta_constraint=None,
+                 gamma_constraint=None,
+                 **kwargs):
+        super(InstanceNormalization, self).__init__(**kwargs)
+        self.supports_masking = True
+        self.axis = axis
+        self.epsilon = epsilon
+        self.center = center
+        self.scale = scale
+        self.beta_initializer = tf.keras.initializers.get(beta_initializer)
+        self.gamma_initializer = tf.keras.initializers.get(gamma_initializer)
+        self.beta_regularizer = tf.keras.regularizers.get(beta_regularizer)
+        self.gamma_regularizer = tf.keras.regularizers.get(gamma_regularizer)
+        self.beta_constraint = tf.keras.constraints.get(beta_constraint)
+        self.gamma_constraint = tf.keras.constraints.get(gamma_constraint)
 
-    def __init__(self, **kwargs):
-        super(InstanceNorm, self).__init__(**kwargs)
+    def build(self, input_shape):
+        ndim = len(input_shape)
+        if self.axis == 0:
+            raise ValueError('Axis cannot be zero')
 
-    def compute_output_shape(self, input_shape):
-        return (input_shape[0], input_shape[1]+2, input_shape[2]+2, input_shape[3])
+        if (self.axis is not None) and (ndim == 2):
+            raise ValueError('Cannot specify axis for rank 1 tensor')
 
-    def call(self, x):
-        return tf.contrib.layers.instance_norm(x,
-                                               epsilon=1e-05,
-                                               center=True, scale=True)
-        
+        self.input_spec = tf.keras.layers.InputSpec(ndim=ndim)
 
+        if self.axis is None:
+            shape = (1,)
+        else:
+            shape = (input_shape[self.axis],)
 
+        if self.scale:
+            self.gamma = self.add_weight(shape=shape,
+                                         name='gamma',
+                                         initializer=self.gamma_initializer,
+                                         regularizer=self.gamma_regularizer,
+                                         constraint=self.gamma_constraint)
+        else:
+            self.gamma = None
+        if self.center:
+            self.beta = self.add_weight(shape=shape,
+                                        name='beta',
+                                        initializer=self.beta_initializer,
+                                        regularizer=self.beta_regularizer,
+                                        constraint=self.beta_constraint)
+        else:
+            self.beta = None
+        self.built = True
+
+    def call(self, inputs, training=None):
+        input_shape = tf.keras.backend.int_shape(inputs)
+        reduction_axes = list(range(0, len(input_shape)))
+
+        if self.axis is not None:
+            del reduction_axes[self.axis]
+
+        del reduction_axes[0]
+
+        mean = tf.keras.backend.mean(inputs, reduction_axes, keepdims=True)
+        stddev = tf.keras.backend.std(inputs, reduction_axes, keepdims=True) + self.epsilon
+        normed = (inputs - mean) / stddev
+
+        broadcast_shape = [1] * len(input_shape)
+        if self.axis is not None:
+            broadcast_shape[self.axis] = input_shape[self.axis]
+
+        if self.scale:
+            broadcast_gamma = tf.keras.backend.reshape(self.gamma, broadcast_shape)
+            normed = normed * broadcast_gamma
+        if self.center:
+            broadcast_beta = tf.keras.backend.reshape(self.beta, broadcast_shape)
+            normed = normed + broadcast_beta
+        return normed
+
+    def get_config(self):
+        config = {
+            'axis': self.axis,
+            'epsilon': self.epsilon,
+            'center': self.center,
+            'scale': self.scale,
+            'beta_initializer': tf.keras.initializers.serialize(self.beta_initializer),
+            'gamma_initializer': tf.keras.initializers.serialize(self.gamma_initializer),
+            'beta_regularizer': tf.keras.regularizers.serialize(self.beta_regularizer),
+            'gamma_regularizer': tf.keras.regularizers.serialize(self.gamma_regularizer),
+            'beta_constraint': tf.keras.constraints.serialize(self.beta_constraint),
+            'gamma_constraint': tf.keras.constraints.serialize(self.gamma_constraint)
+        }
+        base_config = super(InstanceNormalization, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))        
+
+    
 class AdaIN(Layer):
     def __init__(self, alpha=1.0, **kwargs):
         self.alpha = alpha
@@ -94,4 +218,51 @@ class AdaIN(Layer):
                                                                 tf.sqrt(style_variance), epsilon)
         normalized_content_features = self.alpha * normalized_content_features + (1 - self.alpha) * content_features
         return normalized_content_features
+
+
+if USE_TF_KERAS:
+    Input = tf.keras.layers.Input
+    Conv2D = tf.keras.layers.Conv2D
+    DepthwiseConv2D = tf.keras.layers.DepthwiseConv2D
+    BatchNormalization = tf.keras.layers.BatchNormalization
+    Activation = tf.keras.layers.Activation
+    MaxPooling2D = tf.keras.layers.MaxPooling2D
+    Layer = tf.keras.layers.Layer
+    Model = tf.keras.models.Model
+    VGG19 = tf.keras.applications.vgg19.VGG19
+else:
+    Input = keras.layers.Input
+    Conv2D = keras.layers.Conv2D
+    DepthwiseConv2D = keras.layers.DepthwiseConv2D
+    BatchNormalization = keras.layers.BatchNormalization
+    Activation = keras.layers.Activation
+    MaxPooling2D = keras.layers.MaxPooling2D
+    Layer = keras.layers.Layer
+    Model = keras.models.Model
+    VGG19 = keras.applications.vgg19.VGG19
+
+def cartoon_generator(input_size=256):
+
+    input_shape=[input_size,input_size,3]
+    
+    x = Input(shape=input_shape, name="input")
+    img_input = x
+
+    # Block 1
+    x = SpatialReflectionPadding(3)(x)
+    x = Conv2D(64, (7, 7), strides=1, use_bias=True, padding='valid')(x)
+    x = InstanceNormalization()(x)
+    x = Activation("relu")(x)
+    # (112,112,32)
+
+    model = Model(img_input, x, name='cartoon_generator')
+    # model.load_weights(h5_fname)
+    return model
+
+if __name__ == '__main__':
+    
+    model = cartoon_generator(input_size=256)
+    model.summary()
+    
+
 
