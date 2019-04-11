c29fec653ab3818adae8a4f62ff9328baf4f0324
jeongjoonsup <penny4860@gmail.com>
2019-04-11 21:55:38 +0900

    Hosoda params saved

--- net.py
+++ net.py
@@ -7,7 +7,7 @@ def load_model():
     from cartoon import MODEL_ROOT
     import torch
     model = Transformer()
-    model.load_state_dict(torch.load(os.path.join(MODEL_ROOT, "Shinkai_net_G_float.pth")))
+    model.load_state_dict(torch.load(os.path.join(MODEL_ROOT, "Hosoda_net_G_float.pth")))
     model.eval()
     model.float()
     return model
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     params = []
     for i, p in enumerate(model.parameters()):
         param = p.data.numpy()
-        np.save("Shinkai/{}".format(i), param)
+        np.save("Hosoda/{}".format(i), param)
         print(i, param.shape)
 
 
