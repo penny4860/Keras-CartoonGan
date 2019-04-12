cbc897bfd97c91a3ba5aec8bd0cdc33de84c7408
jeongjoonsup <penny4860@gmail.com>
2019-04-12 20:39:58 +0900

    inference main driver done

--- main.py
+++ main.py
@@ -6,31 +6,44 @@ from cartoon.utils import load_net_in
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 
 INPUT_IMG_FNAME = "sample_in/sjtu.jpg"
 INPUT_SIZE = 512
 
+argparser = argparse.ArgumentParser(description='CartoonGan')
+argparser.add_argument(
+    '-i',
+    '--input_img_fname',
+    default=INPUT_IMG_FNAME,
+    help='input image filename')
+argparser.add_argument(
+    '-s',
+    '--input_size',
+    default=INPUT_SIZE,
+    help='generator network input size')
+
+
 if __name__ == '__main__':
+    args = argparser.parse_args()
     model_names = ["params/Hayao.h5", "params/Hosoda.h5", "params/Paprika.h5", "params/Shinkai.h5"]
-    
-    model = cartoon_generator(input_size=INPUT_SIZE)
+    model = cartoon_generator(input_size=args.input_size)
 
     fig, ax = plt.subplots()
     plt.subplot(1, 5, 1)
     plt.axis('off')
     plt.title("input photo")
-    plt.imshow(postprocess(load_net_in(INPUT_IMG_FNAME)))
+    plt.imshow(postprocess(load_net_in(args.input_img_fname)))
 
     for j, model_path in enumerate(model_names):
         model.load_weights(model_path)
     
-        imgs = np.expand_dims(load_net_in(INPUT_IMG_FNAME, desired_size=INPUT_SIZE), axis=0)
+        imgs = np.expand_dims(load_net_in(args.input_img_fname, desired_size=args.input_size), axis=0)
         ys = model.predict(imgs)
         y = postprocess(ys)[0]
 
         plt.subplot(1, 5, j+2)
         plt.axis('off')
-        plt.title(os.path.basename(model_path))
+        plt.title(os.path.basename(model_path).split(".")[0])
         plt.imshow(y)
-    plt.subplots_adjust(bottom = 0, top = 1, hspace = 0.01, wspace = 0.01)
     plt.show()
