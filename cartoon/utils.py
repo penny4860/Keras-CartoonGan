698352ac87336f72f4632215c866d025ba6a079c
jeongjoonsup <penny4860@gmail.com>
2019-04-13 18:00:11 +0900

    error fixed

--- cartoon/utils.py
+++ cartoon/utils.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import os
 from PIL import Image
 from cartoon import SAMPLE_IMG
 
@@ -17,27 +16,4 @@ def load_net_in(img_fname=SAMPLE_IMG, desired_size=256):
     input_image = -1 + 2 * input_image
     return input_image
 
-def run_by_torch(input_image, model_name="Hayao"):
-    import torch
-    from cartoon import MODEL_ROOT
-    def load_model():
-        model = Transformer()
-        model.load_state_dict(torch.load(os.path.join(MODEL_ROOT, '{}_net_G_float.pth'.format(model_name))))
-        model.eval()
-        model.float()
-        return model
-    from torch.autograd import Variable
-    from network.Transformer import Transformer
-    import torchvision.transforms as transforms
-    
-    model = load_model()
-    input_image = transforms.ToTensor()(input_image).unsqueeze(0)
-    input_image = Variable(input_image, volatile=True).float()
-  
-    # forward
-    ys = model(input_image)
-    ys = ys.data.cpu().float()
-    ys = ys.numpy()
-    ys = np.transpose(ys, [0, 2, 3, 1])
-    return ys
     
