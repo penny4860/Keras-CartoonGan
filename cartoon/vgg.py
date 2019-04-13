0c46ceed14961fd298574dea20b910992de9c139
jeongjoonsup <penny4860@gmail.com>
2019-04-13 19:49:32 +0900

    vgg_feat_extractor : 전처리 layer까지 포함

--- cartoon/vgg.py
+++ cartoon/vgg.py
@@ -1,31 +1,31 @@
 # -*- coding: utf-8 -*-
 
 import tensorflow as tf
+from cartoon.layers import VggPreprocess
 
 tf.keras.applications.vgg19.preprocess_input
-
 Model = tf.keras.models.Model
 
-def vgg_feat_extractor():
-    model = tf.keras.applications.vgg19.VGG19(weights='imagenet')
-    model = Model(model.input,
-                  model.get_layer('block4_conv4').output,
-                  name='vgg_feat_extractor')
+def vgg_feat_extractor(input_size=256):
+    def _build_model(input_shape):
+        img_input = tf.keras.layers.Input(shape=input_shape, name="input")
+        
+        x = VggPreprocess()(img_input)
+        base_model = tf.keras.applications.vgg19.VGG19(weights='imagenet')
+        model = Model(inputs=base_model.input, outputs=base_model.get_layer('block4_conv4').output)
+        x = model(x)
+
+        feat_model = Model(img_input, x, name='vgg_feat_extractor')
+        return feat_model
+    input_shape=[input_size,input_size,3]
+    model = _build_model(input_shape)
     model.trainable = False
     return model
-
-def vgg_preprocess(xs):
-    # xs : bgr-ordered, [0, 1]-ranged
-    xs = xs * 255
-    mean = [103.939, 116.779, 123.68]
-    xs[..., 0] -= mean[0]
-    xs[..., 1] -= mean[1]
-    xs[..., 2] -= mean[2]
-    return xs
 
 
 if __name__ == '__main__':
     model = vgg_feat_extractor()
+    model.summary()
     # model.predict(np.zeros((1,224,224,3)))
 
     
