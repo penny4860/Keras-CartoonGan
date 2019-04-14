349f9aa190cc787c943c7d67c9346d3ef90614a5
jeongjoonsup <penny4860@gmail.com>
2019-04-14 12:37:47 +0900

    create_smooth_dataset() added

--- cartoon/utils.py
+++ cartoon/utils.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import cv2, os
 from PIL import Image
 from cartoon import SAMPLE_IMG
+from tqdm import tqdm
+
 
 def load_net_in(img_fname=SAMPLE_IMG, desired_size=256):
     input_image = Image.open(img_fname).convert("RGB")
@@ -39,4 +42,30 @@ def postprocess(ys):
     # [0, 1]-range
     ys = ys * 0.5 + 0.5
     return ys
+
+def create_smooth_dataset(root, save):
+    file_list = os.listdir(root)
+    if not os.path.isdir(save):
+        os.makedirs(save)
+    kernel_size = 5
+    kernel = np.ones((kernel_size, kernel_size), np.uint8)
+    gauss = cv2.getGaussianKernel(kernel_size, 0)
+    gauss = gauss * gauss.transpose(1, 0)
+    for n, f in tqdm(enumerate(file_list)):
+        rgb_img = cv2.imread(os.path.join(root, f))
+        gray_img = cv2.imread(os.path.join(root, f), 0)
+        rgb_img = cv2.resize(rgb_img, (256, 256))
+        pad_img = np.pad(rgb_img, ((2,2), (2,2), (0,0)), mode='reflect')
+        gray_img = cv2.resize(gray_img, (256, 256))
+        edges = cv2.Canny(gray_img, 100, 200)
+        dilation = cv2.dilate(edges, kernel)
+
+        gauss_img = np.copy(rgb_img)
+        idx = np.where(dilation != 0)
+        for i in range(np.sum(dilation != 0)):
+            gauss_img[idx[0][i], idx[1][i], 0] = np.sum(np.multiply(pad_img[idx[0][i]:idx[0][i] + kernel_size, idx[1][i]:idx[1][i] + kernel_size, 0], gauss))
+            gauss_img[idx[0][i], idx[1][i], 1] = np.sum(np.multiply(pad_img[idx[0][i]:idx[0][i] + kernel_size, idx[1][i]:idx[1][i] + kernel_size, 1], gauss))
+            gauss_img[idx[0][i], idx[1][i], 2] = np.sum(np.multiply(pad_img[idx[0][i]:idx[0][i] + kernel_size, idx[1][i]:idx[1][i] + kernel_size, 2], gauss))
+
+        cv2.imwrite(os.path.join(save, str(n) + '.jpg'), gauss_img)
 
